01bdd729161ead0783612c905519f52c3721877f
PureZinc <139257470+PureZinc@users.noreply.github.com>
2024-06-01 15:01:09 -0400

    Added Production to it!

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,17 @@
 from .models import db
 from .email import mail
-from config import config
+from config import config, prod_config
 from flask import Flask
 
 
-def create_app():
+def create_app(production=False):
     app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
-    app.config.update(config)
+    conf = config
+    if production:
+        conf = prod_config
+    
+    app.config.update(conf)
     db.init_app(app)
     mail.init_app(app)
 
--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 # Config for your landing page
+import os
 
 config = {
     'SECRET_KEY': '<your secret key>',
@@ -12,4 +13,18 @@ config = {
     'MAIL_USE_TLS': True,
     'MAIL_USERNAME': 'your_email@example.com',
     'MAIL_PASSWORD': 'your_password'
+}
+
+prod_config = {
+    'SECRET_KEY': os.environ.get('SECRET_KEY'),
+
+    # Database Settings
+    'SQLALCHEMY_DATABASE_URI': 'sqlite:///contacts.db',
+
+    # Email Settings
+    'MAIL_SERVER': 'smtp.gmail.com',
+    'MAIL_PORT': 587,
+    'MAIL_USE_TLS': True,
+    'MAIL_USERNAME': os.environ.get('EMAIL_USERNAME'),
+    'MAIL_PASSWORD': os.environ.get('EMAIL_PASSWORD')
 }
