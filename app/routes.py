6dbb3c09785efb6244ac6a5190e372260930a20f
PureZinc <139257470+PureZinc@users.noreply.github.com>
2024-08-31 20:58:49 -0400

    New Template: Front SaaS - Built with the help of Devslopes. Shoutouts to them!

--- app/routes.py
+++ app/routes.py
@@ -2,32 +2,31 @@ from flask import render_template, request, redirect, url_for, current_app as ap
 from .email import send_contact_message
 
 
-templates = {
-    '1': 'template1.html',
-    '2': 'template2.html'
-}
-
-landing_pages = [
-    {
+landing_pages = {
+    'the-supermarket' : {
         'name': 'The Supermarket',
-        'template': '1'
+        'template': 'template1.html'
     },
-    {
+    'digital-agents' : {
         'name': 'Digital Agents',
-        'template': '2'
+        'template': 'template2.html'
     },
-]
+    'front-saas' : {
+        'name': 'Front SaaS',
+        'template': 'template3.html'
+    },
+}
 
 
 @app.route('/')
 def pages():
-    return render_template('lists.html', pages=landing_pages)
+    return render_template('lists.html', landing_pages=landing_pages.items())
 
 
-@app.route('/<template>')
-def landing_page(template):
-    if template in templates:
-        return render_template(templates[template])
+@app.route('/<slug>')
+def landing_page(slug):
+    if slug in landing_pages.keys():
+        return render_template('spas/' + landing_pages[slug]['template'])
     else:
         return "Template not found", 404
 
